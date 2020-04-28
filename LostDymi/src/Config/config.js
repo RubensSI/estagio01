8bd82afa97806b2f189f188c5c1ac6357596fa98
mbacefor <mbacefor@gmail.com>
2020-04-27 21:33:34 -0300

    Migração do código

--- LostDymi/src/Config/config.js
+++ LostDymi/src/Config/config.js
@@ -5,5 +5,12 @@ export default {
   autoCenter: Phaser.Scale.CENTER_BOTH,
   parent: 'phaser-example',
   width: 800,
-  height: 600
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 100 },
+      debug: true
+    }
+  }
 };
--- LostDymi/src/Scenes/GameScene.js
+++ LostDymi/src/Scenes/GameScene.js
@@ -1,16 +1,62 @@
 import 'phaser';
+import logo from '../../assets/logo.png';
+import ninjaImage from '../../assets/ninja.png';
+import pole from '../../assets/pole.png';
+import powerbar from '../../assets/powerbar.png';
+
+
+var ninja;
+var ninjaGravity = 800;
+var ninjaJumpPower;
+var score = 0;
+var scoreText;
+var topScore;
+var powerBar;
+var powerTween;
+var placedPoles;
+var poleGroup;
+var minPoleGap = 100;
+var maxPoleGap = 300;
+var ninjaJumping;
+var ninjaFallingDown;
+
 
 export default class GameScene extends Phaser.Scene {
-  constructor () {
+  constructor() {
     super('Game');
   }
 
-  preload () {
+  preload() {
     // load images
-    this.load.image('logo', 'assets/logo.png');
+    this.load.image('logo', logo);
+    this.load.image("ninja", ninjaImage);
+    this.load.image("pole", pole);
+    this.load.image("powerbar", powerbar);
   }
 
-  create () {
-    this.add.image(400, 300, 'logo');
+  create() {
+    this.add.image(400, 300, 'ground');
+    /* this.add.image(400, 300, 'logo');
+    this.add.image(100, 300, 'ninja');
+    this.add.image(200, 300, 'pole');
+    this.add.image(300, 300, 'powerbar'); */
+      ninjaJumping = false;
+			ninjaFallingDown = false;
+			score = 0;
+			placedPoles = 0;
+			poleGroup = this.add.group();
+			topScore = localStorage.getItem("topFlappyScore")==null?0:localStorage.getItem("topFlappyScore");
+			scoreText = this.add.text(10,10,"-",{
+				font:"bold 16px Arial"
+			});
+			//updateScore();
+      //game.stage.backgroundColor = "#87CEEB";
+
+			ninja = this.physics.add.sprite(80,0,"ninja");
+			ninja.setOrigin(0.5);
+      ninja.setBounce(0.08);
+			ninja.lastPole = 1;
+      ninja.body.setGravityY(ninjaGravity);
+      ninja.setCollideWorldBounds(true);
   }
 };
