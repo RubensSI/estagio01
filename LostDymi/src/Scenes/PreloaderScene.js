1737d40bcc52c6100bf242579a4a93824fe4e99f
RubensSI <rubenssi825@gmail.com>
2020-05-30 04:03:58 -0300

    Add game sound

--- LostDymi/src/Scenes/PreloaderScene.js
+++ LostDymi/src/Scenes/PreloaderScene.js
@@ -85,7 +85,7 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image('blueButton2', 'assets/ui/blue_button03.png');
     this.load.image('box', 'assets/ui/grey_box.png');
     this.load.image('checkedBox', 'assets/ui/blue_boxCheckmark.png');
-    this.load.audio('bgMusic', ['assets/TownTheme.mp3']);
+    this.load.audio('bgMusic', ['assets/sound/oedipus_ark_pandora.mp3']);
   }
 
   ready () {
